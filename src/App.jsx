96534aac2aec91be682f26a6030204476dd310f8
p1xan <pixan888@gmail.com>
2021-09-20 17:26:31 +0300

    HOC withSuspense, React.lazy, React.Suspense

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
 import {BrowserRouter, Route, withRouter} from "react-router-dom";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
+//import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
+//import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
 import {connect, Provider} from "react-redux";
@@ -12,6 +12,10 @@ import {compose} from "redux";
 import {initializeApp} from "./redux/app-reducer";
 import Preloader from "./components/common/preloader/preloader";
 import store from "./redux/redux-store";
+import {withSuspense} from "./components/hoc/withSuspense";
+
+const ProfileContainer = React.lazy(() => import ("./components/Profile/ProfileContainer"))
+const DialogsContainer = React.lazy(() => import ("./components/Dialogs/DialogsContainer"))
 
 
 class App extends React.Component {
@@ -27,8 +31,8 @@ class App extends React.Component {
                 <div className="contentNavWrapper">
                     <Navbar/>
                     <div className="appWrapperContent">
-                        <Route path="/profile/:userId?" render={() => <ProfileContainer/>}/>
-                        <Route path="/dialogs" render={() => <DialogsContainer/>}/>
+                        <Route path="/profile/:userId?" render={withSuspense(ProfileContainer)}/>
+                        <Route path="/dialogs" render={withSuspense(DialogsContainer)}/>
                         <Route path="/users" render={() => <UsersContainer/>}/>
                         <Route path="/login" render={() => <Login/>}/>
                         <Route path="/news"/>
--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,35 +1,40 @@
 import React, {useState} from "react";
 import styles from "./Paginator.module.css";
 
- let Paginator = (props) => {
+let Paginator = (props) => {
     let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
     let pages = [];
 
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(" " + i)
     }
+
     let portionSize = 10;
     let portionCount = Math.ceil(pagesCount / portionSize);
-    let [portionNumber,setPortionNumber] = useState(1);
-    let leftPortionPageNumber = (portionNumber -1) * portionSize + 1;
+    let [portionNumber, setPortionNumber] = useState(1);
+    let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
     let rightPortionPageNumber = portionNumber * portionSize;
 
     return <div>
         {portionNumber > 1 &&
-            <button className={styles.button} onClick={() => {setPortionNumber(portionNumber - 1)}}>PREV</button>
+        <button className={styles.button} onClick={() => {
+            setPortionNumber(portionNumber - 1)
+        }}>PREV</button>
         }
-            {pages
-                .filter(p => p >= leftPortionPageNumber && p<=rightPortionPageNumber)
-                .map(p => {
+        {pages
+            .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+            .map(p => {
                 return <span key={p.id} className={props.currentPage === p && styles.selectedPage}
                              onClick={(e) => {
                                  props.onPageChanged(p)
                              }}>{p}</span>
             })}
         {portionCount > portionNumber &&
-        <button className={styles.button} onClick={() => {setPortionNumber(portionNumber + 1)}}>NEXT</button>
+        <button className={styles.button} onClick={() => {
+            setPortionNumber(portionNumber + 1)
+        }}>NEXT</button>
         }
-        </div>
+    </div>
 }
 
 export default Paginator;
