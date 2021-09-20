8a4e2a60548bcf2ce51f59e4bd5912a71a543c1a
p1xan <pixan888@gmail.com>
2021-09-20 16:24:30 +0300

    fix paginator

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,21 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./Paginator.module.css";
 
  let Paginator = (props) => {
     let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
     let pages = [];
+
     for (let i = 1; i <= pagesCount; i++) {
-        pages.push(i)
+        pages.push(" " + i)
     }
+    let portionSize = 10;
+    let portionCount = Math.ceil(pagesCount / portionSize);
+    let [portionNumber,setPortionNumber] = useState(1);
+    let leftPortionPageNumber = (portionNumber -1) * portionSize + 1;
+    let rightPortionPageNumber = portionNumber * portionSize;
+
     return <div>
-            {pages.map(p => {
+        {portionNumber > 1 &&
+            <button className={styles.button} onClick={() => {setPortionNumber(portionNumber - 1)}}>PREV</button>
+        }
+            {pages
+                .filter(p => p >= leftPortionPageNumber && p<=rightPortionPageNumber)
+                .map(p => {
                 return <span key={p.id} className={props.currentPage === p && styles.selectedPage}
                              onClick={(e) => {
                                  props.onPageChanged(p)
                              }}>{p}</span>
             })}
+        {portionCount > portionNumber &&
+        <button className={styles.button} onClick={() => {setPortionNumber(portionNumber + 1)}}>NEXT</button>
+        }
         </div>
-
 }
 
 export default Paginator;
